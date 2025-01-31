3fdc89bff6b0b9cd837aead134fd29a3d4e99939
Nikol_Niven <ntopalusic@gmail.com>
2025-01-31 14:05:22 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
+import React, { useState } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
 import ExponentTwo from "./components/ExponentTwo";
 import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import Counter from "./components/Counter"; // Ensure Counter is imported
 
+function App() {
+  const [count, setCount] = useState(0);
 
-function App () {
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} increment={increment} decrement={decrement} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
       </div>
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
 
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-  const increment = () => setCount((prevCount) => prevCount + 1);
+//   const decrement = () => setCount((prevCount) => prevCount - 1);
+//   const increment = () => setCount((prevCount) => prevCount + 1);
+
+
+function Counter({count, increment, decrement}){
 
   return (
     <div className="counter-container">
